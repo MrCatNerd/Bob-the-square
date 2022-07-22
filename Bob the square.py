804f1d159793b8b310d59308f9812a35f3998363
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2022-07-22 11:12:07 +0300

    switching to the DOUBLEBUF drawing method
    using
    self.app = pygame.dispaly.set_mode((self.WIDTH,self.HEIGHT))
    but now using
    self.app = pygame.display.set_mode((self.WIDTH,self.HEIGHT),pyagme,DOUBLEBUF)
    
    and in pygame.display.update()
    but now using
    pygame.display.flip()

--- Bob the square.py
+++ Bob the square.py
@@ -173,7 +173,7 @@ class Game:
         self.level = 1
         self.stars = 0
 
-        self.app = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.app = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF)
         pygame.display.set_caption(self.TITLE)
         pygame.display.set_icon(LOGO)
 
@@ -472,7 +472,7 @@ class Game:
                     self.coins_handler.write_new(self.coins)
                     stop_executing()
             self.clock.tick(FPS)
-            pygame.display.update()
+            pygame.display.flip()
             if len(self.world.enemy_data) > 0:
                 for enemy in self.world.enemy_data:
                     if enemy.out_of_lives:
