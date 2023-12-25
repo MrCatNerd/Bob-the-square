2c6867650ecc0d4cd755b56ebbf355a53fe91105
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2023-12-25 18:22:26 +0200

    added gitignore Closes #7

--- coins_handler.py
+++ coins_handler.py
@@ -1,31 +1,40 @@
-__author__ = "Alon B.R."
-
-from os.path import join
+import os
 
 
 class HandleCoins:
     def __init__(self) -> None:
-        self.path: str = join("Assets", "coins.txt")
-        with open(self.path, "r+") as coins_file:
-            coins_file.seek(0)
-            coins = coins_file.read()
-            if len(coins) > 0:
-                coins = int(coins)
-                self.coins = coins
-            else:
-                coins_file.seek(0)
-                coins_file.truncate()
+        self.path: str = "Assets/coins.txt"
+        self.create_file_if_not_exists()  # Create file if it doesn't exist
+        self.read_coins()
+
+    def create_file_if_not_exists(self) -> None:
+        if not os.path.exists(self.path):
+            with open(self.path, "w") as coins_file:
                 coins_file.write("0")
+
+    def read_coins(self) -> None:
+        with open(self.path, "r") as coins_file:
+            coins = coins_file.read().strip()  # Remove whitespace characters
+            if coins.isdigit():
+                self.coins = int(coins)
+            else:
                 self.coins = 0
 
     def write_new(self, coins) -> None:
-        if self.coins < int(coins):
+        coins = int(coins)
+        if self.coins < coins:
             with open(self.path, "w") as coins_file:
-                coins_file.seek(0)
-                coins_file.truncate()
                 coins_file.write(str(coins))
                 self.coins = coins
 
 
 if __name__ == "__main__":
     handle_coins = HandleCoins()
+
+    # Example usage:
+    print("Current coins:", handle_coins.coins)
+
+    new_coins = input("Enter new coins: ")
+    handle_coins.write_new(new_coins)
+
+    print("Updated coins:", handle_coins.coins)
